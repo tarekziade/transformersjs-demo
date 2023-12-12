baf96c7983e9db996e93d9d2f7a0c0f88e09a2a4
Tarek Ziadé <tarek@ziade.org>
2023-12-12 11:30:37 +0100

    use a wikipedia page

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-import { pipeline } from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.3.0";
+import { pipeline } from "https://cdn.jsdelivr.net/npm/@xenova/transformers";
 
-const longTextInput = document.getElementById("long-text-input");
 const generateButton = document.getElementById("generate-button");
-const output = document.getElementById("output-div");
+const output = document.getElementById("summary-output");
 const spinner = document.getElementById("spinner");
+const stats = document.getElementById("summary-stats");
 
 const summarization = await pipeline(
   "summarization", // task
   "Xenova/t5-small", // model
 );
+
+function cleanOutput(text) {
+  const sentences = text.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/g);
+
+  const capitalizedSentences = sentences.map((sentence) => {
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+  });
+
+  return capitalizedSentences.join(" ");
+}
 
 generateButton.removeAttribute("disabled");
 
@@ -16,19 +26,31 @@ generateButton.addEventListener("click", async () => {
   spinner.classList.add("show");
   generateButton.setAttribute("disabled", true);
 
-  const input = longTextInput.value;
+  console.log("Extracting article with readability...");
+  var iframe = document.getElementById("article");
+  var iframeWindow = iframe.contentWindow || iframe.contentDocument;
+  var documentClone = iframeWindow.document.cloneNode(true);
+
+  const input = new Readability(documentClone)
+    .parse()
+    .textContent.replace(/\s+/g, " ")
+    .trim();
+
+  console.log(input);
   const start = Date.now();
   var stext = `Text length: ${input.length}. `;
+
+  console.log("Summarizing...");
 
   const result = await summarization(input, {
     min_length: 50,
     max_length: 250,
   });
 
-  stext += `Result length: ${result[0].summary_text.length}. `;
-  output.innerHTML = result[0].summary_text;
-
   const end = Date.now();
+  stext += `Result length: ${result[0].summary_text.length}. `;
+  output.innerHTML = cleanOutput(result[0].summary_text);
+
   stext += `Execution time: ${end - start} ms.`;
 
   stats.innerHTML = stext;
